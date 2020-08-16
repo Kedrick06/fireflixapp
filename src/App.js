cabd26c9a10f866e8987ca648d2a2851b993f202
Kedrick06 <bfloblk@gmail.com>
2020-08-16 18:13:27 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Row from './Components/Row'
 
 function App() {
   return (
     <div className="App">
-    
+    <h1> Welcome to FireFlix</h1>
+    <Row title="FireFlix Originals" />
+    <Row title="Trending Now" />
     </div>
   );
 }
