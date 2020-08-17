4cb73242cb8e94b7f6332742d2d5fc8e67f6b1b7
Kedrick06 <bfloblk@gmail.com>
2020-08-17 14:50:30 -0500

    commit

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,15 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
+import Axios from './Axios';
+import requests from './requests';
 
 
 class Banner extends Component{
 
     render(){
+        const [movie, setMovie] = useState([]);
+
+        useEffect (()  => {
+            async function fetchData() {
+
+            }
+
+            fetchData();
+        }, []);
+
+
+
 
         return(
+            <header>
 
-            <div className="banner-css">
-            
-            </div>
+            </header>
         )
     }
 }
