9ca4dcd8553504f05eab6d610fc9535b525b757d
Kedrick06 <bfloblk@gmail.com>
2020-08-17 16:09:39 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Display from './Components/Display'
 import Banner from './Components/Banner'
+import Nav from './Components/Nav'
 
 
 function App() {
   return (
     <div className="App">
+    <Nav />
     <Banner />
     <Display />
     </div>
---
+++ src/Components/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Nav extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+        return(
+            <div className="nav-content">
+                <img className="nav-logo"
+                src="https://toppng.com/uploads/preview/transparent-flame-logo-flame-logo-115630004981ut5wwx6kt.png"
+                alt="FireFlix Logo"/>
+            </div>
+        )
+    }
+
+}
+
+export default Nav;
