778fed2f4da35939d7593291900d64717da3cbd7
Kedrick06 <bfloblk@gmail.com>
2020-08-18 12:58:54 -0500

    Commit

--- src/Components/requests.js
+++ src/Components/requests.js
@@ -2,6 +2,7 @@ const API_KEY = "6584e5c12c22b13251ad3db1625614a8";
 
 
 const requests = {
+
 fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
 fetchFireflixOriginals: `/discover/tv?api_key${API_KEY}&with_networks=213`,
 fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
@@ -10,6 +11,7 @@ fetComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
 fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
 fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
 fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+
 }
 
 export default requests;
